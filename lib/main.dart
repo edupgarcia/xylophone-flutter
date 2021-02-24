975491494db5bfbf8855129d093c54bf97cc9196
Eduardo Pereira Garcia <edupgarcia.ti@gmail.com>
2021-02-24 08:23:08 -0300

    Under construction, backup for HD formatting.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,55 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final colorArray = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple
+  ];
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  void buildKey(int soundNumber) {
+    return Expanded(
+      child: ColoredBox(
+        color: Colors.red,
+        child: TextButton(
+          onPressed: () {
+            playSound(soundNumber);
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1),
+              buildKey(2),
+              buildKey(3),
+              buildKey(4),
+              buildKey(5),
+              buildKey(6),
+              buildKey(7),
+            ],
+          ),
         ),
       ),
     );
